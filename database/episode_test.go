f99c147b48ad2944c130460a582d43bc54664c08
Marcus Willock <crazcalm@gmail.com>
2018-05-14 12:57:17 -0400

    Added tests for: - FormatEpisodeHeader - GetEpisodeHeaderData

--- database/episode_test.go
+++ database/episode_test.go
@@ -12,6 +12,36 @@ func getTestAnswer(path []string) string {
 	return string(file)
 }
 
+func TestFormatEpisodeHeader(t *testing.T) {
+	feedName := "feedName"
+	epTitle := "Episode Title"
+	author := "Jane Doe"
+	date := "Jan 01, 2045"
+	mediaContent := "link to media"
+	epLink := "Episode link"
+
+	tests := []struct {
+		FeedName     string
+		EpTitle      string
+		Author       string
+		EpLink       string
+		Date         string
+		MediaContent string
+		Expect       string
+	}{
+		{"", "", "", "", "", "", ""},
+		{feedName, epTitle, author, epLink, date, mediaContent,
+			"Feed: feedName\nTitle: Episode Title\nAuthor: Jane Doe\nLink: Episode link\nDate: Jan 01, 2045\nMedia Link: link to media\n"},
+	}
+
+	for _, test := range tests {
+		result := FormatEpisodeHeader(test.FeedName, test.EpTitle, test.Author, test.EpLink, test.Date, test.MediaContent)
+		if !strings.EqualFold(result, test.Expect) {
+			t.Errorf("Expected %s, but got %s", test.Expect, result)
+		}
+	}
+}
+
 func TestHeader(t *testing.T) {
 	tests := []struct {
 		Feed       *Feed
--- database/feed_and_episodes_test.go
+++ database/feed_and_episodes_test.go
@@ -1,9 +1,136 @@
 package database
 
 import (
+	"fmt"
+	"strings"
 	"testing"
 	"time"
 )
+
+func TestGetEpisodeHeaderData(t *testing.T) {
+	testName := "get_episode_header_data"
+	file := fmt.Sprintf("./testing/%s.db", testName)
+	db := createTestDB(file)
+
+	feedName := "feed name"
+	feedName2 := "feed name 2"
+	feedURL := fmt.Sprintf("%s.com", testName)
+	feedURL2 := fmt.Sprintf("%s.zh", testName)
+	epTitle1 := "episode title 1"
+	epURL1 := fmt.Sprintf("%s/1", feedURL)
+	epTitle2 := "episode title 2"
+	epURL2 := fmt.Sprintf("%s/2", feedURL)
+	epTitle3 := "episode title 3"
+	epURL3 := fmt.Sprintf("%s/3", feedURL)
+	feedAuthorName := "feed author name"
+	feedAuthorEmail := "feed author email"
+	epAuthorName := "episode author name"
+	epAuthorEmail := "episode author email"
+	mediaContent := "media content"
+	rawData := "raw Data"
+	date := time.Now()
+
+	feedID, err := AddFeedURL(db, feedURL)
+	if err != nil {
+		t.Error(err)
+	}
+
+	feedID2, err := AddFeedURL(db, feedURL2)
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = UpdateFeedTitle(db, feedID, feedName)
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = UpdateFeedTitle(db, feedID2, feedName2)
+	if err != nil {
+		t.Error(err)
+	}
+
+	feedAuthorID, err := AddAuthor(db, feedAuthorName, feedAuthorEmail)
+	if err != nil {
+		t.Error(err)
+	}
+
+	epAuthorID, err := AddAuthor(db, epAuthorName, epAuthorEmail)
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = UpdateFeedAuthor(db, feedID, feedAuthorID)
+	if err != nil {
+		t.Error(err)
+	}
+
+	epID1, err := AddEpisode(db, feedID, epURL1, epTitle1, &date, rawData)
+	if err != nil {
+		t.Errorf("Failed to add an episode to the database: %s", err.Error())
+	}
+
+	err = UpdateEpisodeAuthor(db, epID1, epAuthorID)
+	if err != nil {
+		t.Error(err)
+	}
+
+	epID2, err := AddEpisode(db, feedID, epURL2, epTitle2, &date, rawData)
+	if err != nil {
+		t.Errorf("Failed to add an episode to the database: %s", err.Error())
+	}
+
+	err = UpdateEpisodeMediaContent(db, epID2, mediaContent)
+	if err != nil {
+		t.Error(err)
+	}
+
+	epID3, err := AddEpisode(db, feedID2, epURL3, epTitle3, &date, rawData)
+	if err != nil {
+		t.Errorf("Failed to add an episode to the database: %s", err.Error())
+	}
+
+	tests := []struct {
+		FeedID       int64
+		EpisodeID    int64
+		FeedName     string
+		EpTitle      string
+		Author       string
+		EpLink       string
+		MediaContent string
+	}{
+		{feedID, epID1, feedName, epTitle1, "episode author name (episode author email)", epURL1, ""},
+		{feedID, epID2, feedName, epTitle2, "feed author name (feed author email)", epURL2, mediaContent},
+		{feedID2, epID3, feedName2, epTitle3, "", epURL3, ""},
+	}
+
+	for _, test := range tests {
+		dbFeedName, dbEpTitle, dbAuthor, dbEpLink, _, dbMediaContent, err := GetEpisodeHeaderData(db, test.FeedID, test.EpisodeID)
+		if err != nil {
+			t.Errorf("Unexpected error: %s", err.Error())
+		}
+
+		if !strings.EqualFold(dbFeedName, test.FeedName) {
+			t.Errorf("(epID = %d) Expected %s, but got %s", test.EpisodeID, test.FeedName, dbFeedName)
+
+		}
+		if !strings.EqualFold(dbEpTitle, test.EpTitle) {
+			t.Errorf("(epID = %d) Expected %s, but got %s", test.EpisodeID, test.EpTitle, dbEpTitle)
+		}
+
+		if !strings.EqualFold(dbAuthor, test.Author) {
+			t.Errorf("(epID = %d) Expected %s, but got %s", test.EpisodeID, test.Author, dbAuthor)
+		}
+
+		if !strings.EqualFold(dbEpLink, test.EpLink) {
+			t.Errorf("(epID = %d) Expected %s, but got %s", test.EpisodeID, test.EpLink, dbEpLink)
+		}
+
+		if !strings.EqualFold(dbMediaContent, test.MediaContent) {
+			t.Errorf("(epID = %d) Expected %s, but got %s", test.EpisodeID, dbMediaContent, test.MediaContent)
+		}
+	}
+}
 
 func TestGetEpisodeIDByFeedIDAndTitle(t *testing.T) {
 	file := "./testing/get_episode_id_by_feed_id_and_title.db"
