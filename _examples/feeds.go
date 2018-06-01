b5b3144ce354739db3e42ee17ae0a54698d95011
Marcus Willock <crazcalm@gmail.com>
2018-06-01 18:53:10 -0400

    Added the ability to pass in a path to your database or where you would like a database created

--- _examples/feeds.go
+++ _examples/feeds.go
@@ -1,14 +1,27 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
+	"strings"
 
 	"github.com/jroimartin/gocui"
 
 	"github.com/crazcalm/go-rss-reader"
+	"github.com/crazcalm/go-rss-reader/database"
 )
 
+var dbPath string
+
 func main() {
+	flag.StringVar(&dbPath, "db", "test.db", "Path the database")
+	flag.Parse()
+	if len(strings.TrimSpace(dbPath)) == 0 {
+		log.Fatal(fmt.Errorf("Did not pass in a database"))
+	}
+	database.DBPath = dbPath
+
 	g, err := gocui.NewGui(gocui.OutputNormal)
 	if err != nil {
 		log.Panicln(err)
